d69e360178a3fb7d22ebe9700a27c026e52091cb
AlexandreCourtin <acourtin@student.42.fr>
2020-12-27 17:54:50 +0100

    inital push

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    public TouchScript touchScript;
+    public bool canMove = false;
+    public float speed = 1f;
+    public float dragPowerX;
+    public float dragPowerY;
+
+    void FixedUpdate() {
+        if (canMove) {
+            Vector2 newPosition = new Vector2(
+                touchScript.correctedInputPosition.x * dragPowerX,
+                touchScript.correctedInputPosition.y * dragPowerY
+            );
+            transform.position = Vector2.Lerp(transform.position, newPosition, speed);
+            transform.rotation = Quaternion.Euler(0f, 0f, -transform.position.x);
+        }
+    }
+}
---
+++ Assets/Scripts/TouchScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour {
+
+    public Vector2 inputPosition;
+    public Vector2 originalInputPosition;
+    public Vector2 correctedInputPosition;
+    public bool touched;
+    public bool touching;
+
+    void Start() {
+        touched = false;
+        touching = false;
+    }
+
+    void Update() {
+        // TOUCH SYSTEM
+		touched = false;
+
+		if (touch() && !touching) {
+			touching = true;
+			touched = true;
+		}
+		else if (!touch() && touching) {
+			touching = false;
+		}
+        
+        // CARD SYSTEM
+        if (touched) {
+            originalInputPosition = inputPosition;
+        }
+
+        if (touching) {
+            correctedInputPosition = new Vector2(
+                inputPosition.x - originalInputPosition.x,
+                inputPosition.y - originalInputPosition.y
+            );
+        } else {
+            correctedInputPosition = Vector2.zero;
+        }
+    }
+
+    private bool touch() {
+		if (Input.GetMouseButton(0)) {
+            inputPosition = Input.mousePosition;
+			return true;
+		} else {
+            for (int i = 0; i < Input.touchCount; i++) {
+                inputPosition = Input.GetTouch(0).position;
+                return true;
+            }
+        }
+		return false;
+	}
+}
