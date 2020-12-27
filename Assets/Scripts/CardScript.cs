45d7da6475ba51043dc665cb30fb5a64a63d5933
AlexandreCourtin <acourtin@student.42.fr>
2020-12-27 19:21:29 +0100

    general loop of app

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -2,22 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardScript : MonoBehaviour
-{
+public class CardScript : MonoBehaviour {
+    public GameObject cardObject;
     public TouchScript touchScript;
-    public bool canMove = false;
+    public CardScript subCard;
+    public Vector3 newPosition;
+    public bool canMove;
     public float speed = 1f;
     public float dragPowerX;
     public float dragPowerY;
 
-    void FixedUpdate() {
-        if (canMove) {
-            Vector2 newPosition = new Vector2(
-                touchScript.correctedInputPosition.x * dragPowerX,
-                touchScript.correctedInputPosition.y * dragPowerY
-            );
-            transform.position = Vector2.Lerp(transform.position, newPosition, speed);
-            transform.rotation = Quaternion.Euler(0f, 0f, -transform.position.x);
+    bool createdSubCard;
+
+    // CALLED WHEN CARD IS SPAWNED
+    void Start() {
+        touchScript = GameObject.Find("Camera").GetComponent<TouchScript>();
+        newPosition = transform.position;
+        createdSubCard = false;
+    }
+
+    // CALLED EVERY TICK
+    void Update() {
+        if (touchScript.touched && !createdSubCard && canMove) {
+            createdSubCard = true;
+            GameObject sub = Instantiate(cardObject, new Vector3(0f, 0f, 5f), Quaternion.identity);
+            sub.name = "Card";
+            subCard = sub.GetComponent<CardScript>();
+            subCard.canMove = false;
         }
+    }
+
+    // CALLED AT A FIXED TIME (every x second)
+    void FixedUpdate() {
+        if (transform.position.x < -1f && canMove && !touchScript.touching) {
+            newPosition = new Vector3(-10f, 0f, 0f);
+            newCardStepForward();
+        } else if (transform.position.x > 1f && canMove && !touchScript.touching) {
+            newPosition = new Vector3(10f, 0f, 0f);
+            newCardStepForward();
+        } else if (canMove) {
+            newPosition = new Vector3(
+                touchScript.correctedInputPosition.x * dragPowerX,
+                touchScript.correctedInputPosition.y * dragPowerY,
+                0f
+            );
+        }
+
+        // Destroy object when reaching border
+        if (transform.position.x < -9 || transform.position.x > 9) {
+            Destroy(this.gameObject);
+        }
+
+        // Always update position and rotation with a Lerp for a smooth effect
+        transform.position = Vector3.Lerp(transform.position, newPosition, speed);
+        transform.rotation = Quaternion.Euler(0f, 0f, -transform.position.x);
+    }
+
+    // CALLED WHEN DISLIKED OR LIKED
+    // DISABLE MOVEMENT AND CALL SUB CARD TO BE THE MAIN CARD
+    private void newCardStepForward() {
+        canMove = false;
+        subCard.canMove = true;
+        subCard.newPosition = Vector3.zero;
     }
 }
--- Assets/Scripts/TouchScript.cs
+++ Assets/Scripts/TouchScript.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class TouchScript : MonoBehaviour {
 
+    public GameObject cardObject;
     public Vector2 inputPosition;
     public Vector2 originalInputPosition;
     public Vector2 correctedInputPosition;
     public bool touched;
     public bool touching;
 
+    // CALLED WHEN APP IS STARTING
     void Start() {
         touched = false;
         touching = false;
+
+        // INSTANTIATE FIRST CARD
+        GameObject sub = Instantiate(cardObject, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        sub.name = "Card";
+        CardScript cardScript = sub.GetComponent<CardScript>();
+        cardScript.canMove = true;
     }
 
+    // CALLED EVERY TICK
     void Update() {
         // TOUCH SYSTEM
 		touched = false;
@@ -27,11 +36,12 @@ public class TouchScript : MonoBehaviour {
 			touching = false;
 		}
         
-        // CARD SYSTEM
+        // GET INITAL POSITION WHEN FIRST TOUCHING SCREEN
         if (touched) {
             originalInputPosition = inputPosition;
         }
 
+        // CORRECT POSITION FROM INITAL TOUCH POSITION
         if (touching) {
             correctedInputPosition = new Vector2(
                 inputPosition.x - originalInputPosition.x,
@@ -42,6 +52,7 @@ public class TouchScript : MonoBehaviour {
         }
     }
 
+    // TOUCHING IS EITHER REAL TOUCH ON MOBILE OR CLICK ON PC
     private bool touch() {
 		if (Input.GetMouseButton(0)) {
             inputPosition = Input.mousePosition;
